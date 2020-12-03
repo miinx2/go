dcbd8c23bd7d037ad5e030fffdcce48d6e18c701
root <root@miinx2.localdomain>
2020-12-03 20:34:20 +0900

    v3 struct

---
+++ go_struct_02.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type part struct {
+	description string
+	count       int
+}
+
+func showInfo(p part) {
+	fmt.Println("Description : ", p.description)
+	fmt.Println("count : ", p.count)
+}
+
+func minimumOrder(description string) part {
+	var p part
+	p.description = description
+	p.count = 1000
+	return p
+}
+
+func main() {
+	p := minimumOrder("Hex bolts")
+	fmt.Println(p.description, p.count)
+
+	//	var bolts part
+	//	bolts.description = "Hex bolts"
+	//	bolts.count = 100
+	//	showInfo(bolts)
+	//	fmt.Println("test")
+}
